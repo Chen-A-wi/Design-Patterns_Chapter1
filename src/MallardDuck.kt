b147a45c3e81b843d4d67c6da8ab39ee7c73f4cb
Ben <benchen@starlux-airlines.com>
2020-06-02 14:21:52 +0800

    [feat] Page 16

--- src/MallardDuck.kt
+++ src/MallardDuck.kt
@@ -1,19 +1,8 @@
 class MallardDuck : Duck() {
-//    override fun quack() {
-//        print("呱呱")
-//    }
-//
-//    override fun swim() {
-//        print("What?!I'm swimming~~")
-//    }
-//
-//    override fun display() {
-//        print("我是綠頭鴨")
-//    }
-//
-//    override fun fly() {
-//        print("I believe i can fly")
-//    }
+    init {
+        quackBehavior = Quack()
+        flyBehavior = FlyWithWings()
+    }
 }
 
 fun main() {
